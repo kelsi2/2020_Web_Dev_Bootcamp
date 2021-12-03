6e4343cd23aa1da23a70dad7537772c8e10f87f3
Kelsi Proulx <kelsiproulx1919@gmail.com>
2021-12-02 18:22:56 -0800

    Add background images to buttons

---
+++ Section 13: Drum Kit/index.js
@@ -0,0 +1,8 @@
+const numberOfDrumButtons = document.querySelectorAll(".drum").length;
+
+for (let i = 0; i < numberOfDrumButtons; i++) {
+  document.querySelectorAll(".drum")[i].addEventListener('click', function() {
+    const audio = new Audio('sounds/tom-1.mp3');
+    audio.play()
+  })
+}
