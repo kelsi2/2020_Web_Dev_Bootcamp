b9304fa17ab24e7ba61500204722388fe45e751d
Kelsi Proulx <kelsiproulx1919@gmail.com>
2021-12-06 16:54:57 -0800

    Add section 18: intro to Node.js

---
+++ Section 18: Node.js/index.js
@@ -0,0 +1,8 @@
+const superheroes = require('superheroes');
+const supervillains = require('supervillains');
+
+const mySuperheroName = superheroes.random();
+const mySupervillainName = supervillains.random();
+
+console.log(mySuperheroName);
+console.log(mySupervillainName);
