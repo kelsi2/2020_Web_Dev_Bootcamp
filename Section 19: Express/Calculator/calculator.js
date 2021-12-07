87d16d95038611579d664341fa9a401098f987c4
Kelsi Proulx <kelsiproulx1919@gmail.com>
2021-12-06 19:23:24 -0800

    Add bmi calculator

--- Section 19: Express/Calculator/calculator.js
+++ Section 19: Express/Calculator/calculator.js
@@ -13,8 +13,20 @@ app.post('/', (req, res) => {
   const num2 = Number(req.body.num2);
   const result = num1 + num2;
 
-  res.send(`The result of the calculation is ${result}`)
-})
+  res.send(`The result of the calculation is ${result}`);
+});
+
+app.get('/bmicalculator', (req, res) => {
+  res.sendFile(`${__dirname}/bmiCalculator.html`);
+});
+
+app.post('/bmicalculator', (req, res) => {
+  const weight = parseFloat(req.body.weight);
+  const height = parseFloat(req.body.height);
+  const bmi = weight / (height * height);
+
+  res.send(`Your BMI is ${bmi}`);
+});
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
