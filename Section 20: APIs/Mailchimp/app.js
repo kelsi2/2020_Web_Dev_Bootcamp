5d89d165d8bfe163ad6951c236c4163011adfd98
Kelsi Proulx <kelsiproulx1919@gmail.com>
2021-12-08 17:22:35 -0800

    Add failure and success pages, and post request

--- Section 20: APIs/Mailchimp/app.js
+++ Section 20: APIs/Mailchimp/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const request = require('request');
+const https = require('https');
 
 const app = express();
 
@@ -16,9 +17,49 @@ app.post('/', (req, res) => {
   const lastName = req.body.lastName;
   const email = req.body.email;
 
-  console.log(firstName, lastName, email)
-})
+  const data = {
+    members: [
+      {
+        email_address: email,
+        status: "subscribed",
+        merge_fields: {
+          FNAME: firstName,
+          LNAME: lastName
+        }
+      }
+    ]
+  };
+
+  const jsonData = JSON.stringify(data);
+
+  const url = "https://us20.api.mailchimp.com/3.0/lists/5efbb9050f";
+
+  const options = {
+    method: "POST",
+    auth: `kelsi:${process.env.MAILCHIMP_API_KEY}`,
+  };
+
+  const request = https.request(url, options, (response) => {
+    if(response.statusCode === 200) {
+      res.sendFile(`${__dirname}/success.html`)
+    } else {
+      res.sendFile(`${__dirname}/failure.html`);
+    }
+
+    response.on("data", (data) => {
+      console.log(JSON.parse(data));
+    });
+  });
+
+  request.write(jsonData);
+
+  request.end();
+});
+
+app.post('/failure', (req, res) => {
+  res.redirect('/');
+});
 
 app.listen(3000, () => {
   console.log('Server is running on port 3000');
-})
+});
