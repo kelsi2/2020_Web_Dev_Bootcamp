67e8b75deccecab30de89b0389438ad5c8d0e127
Kelsi Proulx <kelsiproulx1919@gmail.com>
2022-01-08 11:48:12 -0800

    Add patch method for specific article

--- Section 31: REST/app.js
+++ Section 31: REST/app.js
@@ -73,7 +73,22 @@ app.route('/articles/:articleTitle')
       {title: req.body.title, content: req.body.content},
       (err) => {
         if (!err) {
-          res.send("Successfully updated article.")
+          res.send("Successfully updated article.");
+        } else {
+          res.send(err);
+        }
+      }
+    );
+  })
+  .patch((req, res) => {
+    Article.updateOne(
+      {title: req.params.articleTitle},
+      req.body,
+      (err) => {
+        if (!err) {
+          res.send("Successfully updated article.");
+        } else {
+          res.send(err);
         }
       }
     );
